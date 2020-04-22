1216e1303a6d7e8fca59e6fc48967aabe5449c9f
likhipriya2001@gmail.com <likhipriya2001@gmail.com>
2020-04-22 10:12:36 +0530

    modified css files

--- db/schema.rb
+++ db/schema.rb
@@ -27,20 +27,4 @@ ActiveRecord::Schema.define(version: 2020_04_21_161629) do
     t.string "name"
   end
 
-  create_table "todos", force: :cascade do |t|
-    t.text "todo_text"
-    t.date "due_date"
-    t.boolean "completed"
-    t.bigint "user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
 end
