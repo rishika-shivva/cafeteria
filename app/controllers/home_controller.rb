37d7791d6e6dfae9615da5c0f6cb23eb9133917a
rishika-shivva <s.rishika2001@gmail.com>
2020-05-18 12:05:05 +0530

    Updated

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
 
   def index
     if current_user
-      redirect_to menuitems_path
+      redirect_to menus_path
     else
       redirect_to sessions_path
     end
--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,6 +1,4 @@
 class MenusController < ApplicationController
-  before_action :ensure_owner_logged_in
-
   def index
   end
 
